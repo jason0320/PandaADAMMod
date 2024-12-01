934ddb7c6187a9202c879c513590649b925010ed
User <User@DESKTOP-5EC7HVG>
2024-12-01 21:16:40 +0800

    加入專案檔案。

---
+++ Mod_PandaADAMMod.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Mod_PandaADAMMod 
+{
+    [BepInPlugin("panda.ADAM.mod", "Panda's ADAM Mod", "1.0.0.0")]
+    public class Mod_PandaADAMMod : BaseUnityPlugin
+    {
+        private void Start()
+        {
+            var harmony = new Harmony("Panda's ADAM Mod");
+            harmony.PatchAll();
+        }
+    }
+}
