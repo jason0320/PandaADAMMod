88b2b86bdb554be3752fa1080a1056abc0ca10c1
jason0320 <jasonnosaj0320@gmail.com>
2024-12-27 21:10:25 +0800

    Update

--- FoodEffect_Proc_Patch.cs
+++ FoodEffect_Proc_Patch.cs
@@ -4,17 +4,12 @@ using System.Text;
 using HarmonyLib;
 using UnityEngine;
 
-[HarmonyPatch(typeof(FoodEffect))]
-[HarmonyPatch("Proc")]
-[HarmonyPatch(new Type[]
-{
-	typeof(Chara),
-	typeof(Thing)
-})]
+[HarmonyPatch]
 internal class FoodEffect_Proc_Patch
 {
-
-	public static void Postfix(Chara c, Thing food)
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(FoodEffect), nameof(FoodEffect.Proc))]
+    public static void Postfix(Chara c, Thing food)
 	{
 		bool flag = FoodEffect.IsUndeadFlesh(food);
 		if ((food.IsDecayed || flag) && !c.HasElement(480, 1))
@@ -24,11 +19,11 @@ internal class FoodEffect_Proc_Patch
 		foreach (Element element in food.elements.dict.Values)
 		{
 			List<Element> list = food.ListValidTraits(true, false);
-			if (!element.source.foodEffect.IsEmpty() && list.Contains(element) && element.source.foodEffect[0] == "little" && c.elements.Base(1230) < 10)
+			if (!element.source.foodEffect.IsEmpty() && list.Contains(element) && element.source.foodEffect[0] == "little" && !(c.race.id == "mutant") && c.elements.Base(1230) < 10)
 			{
-				c.Say("little_adam", c, null, null);
-				c.SetFeat(1230, c.elements.Base(1230) + 1, false);
-			}
+				c.Say("little_adam", c);
+                c.SetFeat(1230, c.elements.Base(1230) + 1);
+            }
 		}
 	}
 
